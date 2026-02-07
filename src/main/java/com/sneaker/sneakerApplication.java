93155c19f51d6d4deb287b7c6c03a2483258fb28
kidiezyllex <buitranthienan1111@gmail.com>
2026-02-08 05:01:05 +0700

    chore: add deployment version tag for debugging

--- src/main/java/com/sneaker/sneakerApplication.java
+++ src/main/java/com/sneaker/sneakerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class sneakerApplication {
     public static void main(String[] args) {
+        System.out.println("=== DEPLOYMENT VERSION: 2026-02-08-05-00-v2 ===");
         SpringApplication.run(sneakerApplication.class, args);
     }
 }
